d83ad478199e3ea18a80f7f42eb52fd96dd01bef
student <student@TSC25-GAME02.local>
2025-05-01 15:00:58 +0900

    Initial commit

---
+++ SwiftGameTestTests/SwiftGameTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGameTestTests.swift
+//  SwiftGameTestTests
+//
+//  Created by student on 2025/05/01.
+//
+
+import Testing
+@testable import SwiftGameTest
+
+struct SwiftGameTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
